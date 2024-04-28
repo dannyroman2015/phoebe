1fb61ea8525d6c3f6ae9affea4098a0dd0559cfa
dannyroman2015 <iknowihave@gmail.com>
2024-04-28 21:28:00 +0700

    aaa

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl := template.Must(template.ParseFiles("templates/index.html"))
 		tmpl.Execute(w, nil)
 	})
 
