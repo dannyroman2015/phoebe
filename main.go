f3afa264d54dc0885aa406758f4ffe46b5156d12
dannyroman2015 <iknowihave@gmail.com>
2024-04-28 14:18:14 +0700

    asd

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// mux := http.NewServeMux()
+	// assuming you have a net/http#ServeMux called `mux`
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.Execute(w, nil)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":3000"
+	} else {
+		port = ":" + port
+	}
+	http.ListenAndServe(port, nil)
+}
