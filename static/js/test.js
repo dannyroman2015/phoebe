bf4bd440b7898973ca0bd934e58a0a673b9fed07
dannyroman2015 <iknowihave@gmail.com>
2024-05-18 15:51:37 +0700

    sdkfah

--- static/js/test.js
+++ static/js/test.js
@@ -17,7 +17,120 @@ const formatsInfo = [
 d3.csv("/static/data.csv", d3.autoType).then( data => {
   drawDonutCharts(data);
   drawStackedBars(data);
+  drawStreamGraph(data);
 })
+
+const drawStreamGraph = (data) => {
+  const svg = d3.select("#streamgraph")
+    .append("svg")
+      .attr("viewBox", [0, 0, width, height]);
+
+  const innerChart = svg
+    .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`)
+
+  const stackGenerator = d3.stack()
+    .keys(formatsInfo.map(f => f.id))
+    .order(d3.stackOrderAscending)
+    .offset(d3.stackOffsetSilhouette);
+  
+  const annotatedData = stackGenerator(data)
+  
+  const maxUpperBoundary = d3.max(annotatedData[annotatedData.length - 1], d => d[1])
+
+  const xScale = d3.scaleBand()
+    .domain(data.map(d => d.year))
+    .range([0, innerWidth])
+    .paddingInner(0.2);
+
+    const minLowerBoundaries = []
+    const maxUpperBoundaries = []
+  
+    annotatedData.forEach(series => {
+      minLowerBoundaries.push(d3.min(series, d => d[0]))
+      maxUpperBoundaries.push(d3.max(series, d => d[1]))
+    })
+  
+    const minDomain = d3.min(minLowerBoundaries)
+    const maxDomain = d3.max(maxUpperBoundaries)
+
+  const yScale = d3.scaleLinear()
+    .domain([minDomain, maxDomain])
+    .range([innerHeight, 0])
+    .nice()
+
+  const colorScale = d3.scaleOrdinal()
+    .domain(formatsInfo.map(f => f.id))
+    .range(formatsInfo.map(f => f.color))
+
+  const areaGenerator = d3.area()
+    .x(d => xScale(d.data.year) + xScale.bandwidth()/2)
+    .y0(d => yScale(d[0]))
+    .y1(d => yScale(d[1]))
+    .curve(d3.curveCatmullRom);
+
+    const bottomAxis = d3.axisBottom(xScale)
+    .tickValues(d3.range(1975, 2020, 5))
+    .tickSizeOuter(0)
+
+  innerChart
+    .append("g")
+      .attr("class", "x-axis-streamgraph")
+      .attr("transform", `translate(0, ${innerHeight})`)
+      .call(bottomAxis)
+
+  innerChart
+    .append("g")
+      .attr("class", "areas-container")
+    .selectAll("path")
+    .data(annotatedData)
+    .join("path")
+      .attr("d", areaGenerator)
+      .attr("fill", d => colorScale(d.key));
+
+  const leftAxis = d3.axisLeft(yScale)
+  
+  innerChart
+    .append("g")
+    .call(leftAxis)
+
+  const leftAxisLabel = svg
+    .append("text")
+      .attr("dominant-baseline", "hanging")
+  leftAxisLabel
+    .append("tspan")
+      .text("Total revenue")
+  leftAxisLabel
+    .append("tspan")
+      .text("million USD")
+      .attr("dx", 5)
+      .attr("fill-opacity", 0.5)
+  leftAxisLabel
+    .append("tspan")
+      .text("Adjusted for inflation")
+      .attr("x", 0)
+      .attr("dy", 20)
+      .attr("fill-opacity", 0.5)
+      .attr("font-size", "14px")
+
+  const legendItems = d3.select(".legend-container")  
+    .append("ul")
+      .attr("class", "color-legend")
+    .selectAll(".color-legend-item")
+    .data(formatsInfo)
+    .join("li")
+      .attr("class", "color-legend-item")
+
+  legendItems
+    .append("span")
+      .attr("class", "color-legend-item-color")
+      .style("backgound-color", d => d.color);
+  
+  legendItems
+    .append("text")
+      .attr("class", "color-legend-item-label")
+      .text(d => d.label)
+}
 
 const drawStackedBars = (data) => {
   const svg = d3.select("#bars")
@@ -162,5 +275,4 @@ const drawDonutCharts = (data) => {
           .style("font-weight", 500);
   })
 
-  
 }
