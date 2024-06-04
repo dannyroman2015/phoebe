81896554e2dbfc00a9a9fb29a3f0b277a1b74565
dannyroman2015 <iknowihave@gmail.com>
2024-06-04 16:24:29 +0700

    asdf

--- static/js/test/test3.js
+++ static/js/test/test3.js
@@ -1,6 +1,5 @@
 // import * as go from "../go-debug"
 
-
 const myDiagram = new go.Diagram("myDiagramDiv", 
 {
   "commandHandler.archetypeGroupData": {text: "Group", isGroup: true},
@@ -8,22 +7,45 @@ const myDiagram = new go.Diagram("myDiagramDiv",
   "toolManager.mouseWheelBehavior": go.WheelMode.Zoom,
 });
 
-myDiagram.nodeTemplate = new go.Node("Auto")
-  .add(
-    new go.Shape("RoundedRectangle", {fill: "whitesmoke"}),
-    new go.TextBlock({margin: 5})
-      .bind("text", "someValue")
-  );
+myDiagram.groupTemplate = new go.Group("Auto", {
+  layout: new go.LayeredDigraphLayout({direction: 0, columnSpacing: 10})
+}).add(
+  new go.Shape("RoundedRectangle", {parameter1: 10, fill: "rgba(128,128,128,0.33)"}),
+  new go.Panel("Vertical", {defaultAlignment: go.Spot.Left})
+    .add(
+      new go.Panel("Horizontal", {defaultAlignment: go.Spot.Top}).add(
+        go.GraphObject.make("SubGraphExpanderButton"),
+        new go.TextBlock({font: "Bold 12pt San-serif"}).bind("text")
+      ),
+      new go.Panel("Auto").add(
+        new go.Shape("Rectangle"),
+        new go.TextBlock("skdfhskdh")
+      )
+    )
+)
 
-myDiagram.linkTemplate = new go.Link()
-  .add(new go.Shape(), new go.Shape("OpenTriangle"))
+myDiagram.layout = new go.LayeredDigraphLayout({
+  direction: 90,
+  layerSpacing: 10,
+  isRealtime: false,
+});
 
 myDiagram.model = new go.GraphLinksModel(
   [
-    {key: "Alpha", someValue: 1},
-    {key: "Beta", someValue: 2}
+    {key: 1, text: "Alpha"},
+    {key: 2, text: "Omega", isGroup: true},
+    {key: 3, text: "Beta", group: 2},
+    {key: 4, text: "Gama", group: 2},
+    {key: 5, text: "Epsilon", group: 2},
+    {key: 6, text: "Zeta", group: 2},
+    {key: 7, text: "Delta"}
   ],
   [
-    {from: "Alpha", to: "Beta"}
+    {from: 1, to: 2},
+    {from: 3, to: 4},
+    {from: 3, to: 5},
+    {from: 4, to: 6},
+    {from: 5, to: 6},
+    {from: 2, to: 7},
   ]
 )
